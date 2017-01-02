e0a603b581695ab42af46888730352436d37e81b
lydstyl <lydstyl@gmail.com>
2017-01-02 14:18:54 +0100

    ajout de la grille bootstrap

--- 15_integration/index.php
+++ 15_integration/index.php
@@ -6,9 +6,11 @@
     <meta name="viewport" content="width=device-width, user-scalable=no">
     <link href="../static/css/normalize.css" rel="stylesheet" type="text/css" />
     <link href="../static/css/font-awesome.css" rel="stylesheet" type="text/css" />
-    <link href="../static/css/grille.css" rel="stylesheet" type="text/css" />
-    <link href="css/style.css" rel="stylesheet" type="text/css" />
-    <style></style>
+    <link rel="stylesheet" type="text/css" href="./slick/slick.css">
+    <link rel="stylesheet" type="text/css" href="./slick/slick-theme.css">
+    <link href="./css/bootstrap.css" rel="stylesheet" type="text/css" />
+    <link href="./css/style.css" rel="stylesheet" type="text/css" />
+    <style type="text/css"></style>
   </head>
   <body>
     <div id="page">
@@ -27,11 +29,39 @@
             <div class="clear"></div>
           </div>
         </div>
-        <div></div>
-        <div></div>
+        <div class="regular slider">
+          <div><img src="http://placehold.it/990x300?text=1"></div>
+          <div><img src="http://placehold.it/990x300?text=2"></div>
+          <div><img src="http://placehold.it/990x300?text=3"></div>
+          <div><img src="http://placehold.it/990x300?text=4"></div>
+          <div><img src="http://placehold.it/990x300?text=5"></div>
+        </div>
+        <div class="menu">     
+          <ul>
+            <li>Home</li>
+            <li>Kids</li>
+            <li>Men</li>
+            <li>Women</li>
+            <li>Brands</li>
+            <li>Blog</li>
+            <li>Community</li>
+            <li>Delivery</li>
+            <li>Stores</li>
+            <li>Contacts</li>
+          </ul>
+        </div>
       </header>
       <section>
-        <div></div>
+        <div class="featuredBar">
+          <div class="topBar"></div>
+          <div class="col-md-9 left">
+            <div class="col-md-3"><img src="http://placehold.it/160x150?text=1"></div>
+            <div class="col-md-3"><img src="http://placehold.it/160x150?text=2"></div>
+            <div class="col-md-3"><img src="http://placehold.it/160x150?text=3"></div>
+            <div class="col-md-3"><img src="http://placehold.it/160x150?text=4"></div>
+          </div>
+          <div class="col-md-3 right"><img src="http://placehold.it/250x150?text=5"></div>
+        </div>
         <div></div>
       </section>
       <footer>
@@ -41,4 +71,29 @@
   </body>
   <script type="text/javascript" src="../static/js/jquery.js"></script>
   <script type="text/javascript"></script>
+
+  <script src="https://code.jquery.com/jquery-2.2.0.min.js" type="text/javascript"></script>
+  <script src="./slick/slick.js" type="text/javascript" charset="utf-8"></script>
+  <script type="text/javascript">
+    $(document).on('ready', function() {
+      $(".regular").slick({
+        dots: false,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      });
+      // $(".center").slick({
+      //   dots: true,
+      //   infinite: true,
+      //   centerMode: true,
+      //   slidesToShow: 3,
+      //   slidesToScroll: 3
+      // });
+      // $(".variable").slick({
+      //   dots: true,
+      //   infinite: true,
+      //   variableWidth: true
+      // });
+    });
+  </script>
 </html>

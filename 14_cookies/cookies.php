986f44dbdc82c810383e437991469e539e439104
lydstyl <lydstyl@gmail.com>
2017-01-02 10:21:56 +0100

    integration Versedesign

---
+++ 14_cookies/cookies.php
@@ -0,0 +1,10 @@
+<?php
+
+    //setcookie("gabriel",33,time()+600000);
+    echo $_COOKIE['gabriel'];
+    //setcookie("gabriel",35,time()+600000);
+
+    $actuel = date("H:i:s");
+
+    echo $actuel ;
+?>
---
+++ 15_integration/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Exo PHP</title>
+    <meta name="viewport" content="width=device-width, user-scalable=no">
+    <link href="../static/css/normalize.css" rel="stylesheet" type="text/css" />
+    <link href="../static/css/font-awesome.css" rel="stylesheet" type="text/css" />
+    <link href="../static/css/grille.css" rel="stylesheet" type="text/css" />
+    <link href="css/style.css" rel="stylesheet" type="text/css" />
+    <style></style>
+  </head>
+  <body>
+    <div id="page">
+      <header>
+        <div class="firstBar">
+          <div>            
+            <ul>
+              <li><img src="./img/english.jpg" alt="english flag"></li>
+              <li><span>Good Evening,</span><span> versesdesign</span></li>
+              <li><i class="fa fa-comment"></i></li>
+              <li><i class="fa fa-star"></i></li>
+              <li><i class="fa fa-heart"></i></li>
+              <li><i class="fa fa-shopping-basket"></i></li>
+              <li><i class="fa fa-lock"></i></li>
+            </ul>
+            <div class="clear"></div>
+          </div>
+        </div>
+        <div></div>
+        <div></div>
+      </header>
+      <section>
+        <div></div>
+        <div></div>
+      </section>
+      <footer>
+        
+      </footer>
+    </div>
+  </body>
+  <script type="text/javascript" src="../static/js/jquery.js"></script>
+  <script type="text/javascript"></script>
+</html>

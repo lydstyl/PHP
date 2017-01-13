ac36ed3d125b5a63e40229e9676a167ad7dc43ee
lydstyl <lydstyl@gmail.com>
2017-01-13 16:49:03 +0100

    fin de journée 13-01

--- 17_integration_avec_sql/user.php
+++ 17_integration_avec_sql/user.php
@@ -1,6 +1,10 @@
 <h2>User</h2>
 <?php
-    //print_r($_POST);
+    print_r($_POST);
+    // supprimer
+    if(isset($_POST['type']) && $_POST['type'] == 'supprimer')){
+        mysqli_query($link, 'DELETE FROM `user` WHERE `id`='.$_POST['id']);
+    }
     if(isset($_POST['add']) && $_POST['mail'] != '' && $_POST['pass'] != ''){
         // $resultat = mysqli_query($link, 'SELECT * FROM `user`');
         // $data = mysqli_fetch_assoc($resultat);
@@ -14,3 +18,19 @@
         <input type="text" name="pass" placeholder="mot de passe">
         <input type="submit" name="add" value="Ajouter un utilisateur avec son email">
     </form>
+    <ul>
+<?php
+        $resultat = mysqli_query($link, 'SELECT * FROM `user`');
+        while($data = mysqli_fetch_assoc($resultat)){
+?>
+        <li>
+            <?= $data['mail'] ?>
+            <form method="post" action="admin.php?right=user">
+                <input type="hidden" name="id" value="<?= $data['id'] ?>">
+                <input type="submit" name="type" value="supprimer">
+            </form>
+        </li>
+<?php
+        }
+?>
+    </ul>

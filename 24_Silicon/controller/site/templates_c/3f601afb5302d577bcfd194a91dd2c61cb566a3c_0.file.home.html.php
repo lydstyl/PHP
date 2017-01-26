7119fa29fae64db6ad5b7e784e0d845f87d8e99b
lydstyl <lydstyl@gmail.com>
2017-01-26 10:59:49 +0100

    11h

--- 24_Silicon/controller/site/templates_c/3f601afb5302d577bcfd194a91dd2c61cb566a3c_0.file.home.html.php
+++ 24_Silicon/controller/site/templates_c/3f601afb5302d577bcfd194a91dd2c61cb566a3c_0.file.home.html.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.30, created on 2017-01-26 09:36:22
+/* Smarty version 3.1.30, created on 2017-01-26 09:57:19
   from "C:\wamp64\www\PHP\24_Silicon\view\site\home.html" */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.30',
-  'unifunc' => 'content_5889c316853448_89768585',
+  'unifunc' => 'content_5889c7ff32af40_98890823',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '3f601afb5302d577bcfd194a91dd2c61cb566a3c' => 
     array (
       0 => 'C:\\wamp64\\www\\PHP\\24_Silicon\\view\\site\\home.html',
-      1 => 1485423381,
+      1 => 1485424637,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_5889c316853448_89768585 (Smarty_Internal_Template $_smarty_tpl) {
+function content_5889c7ff32af40_98890823 (Smarty_Internal_Template $_smarty_tpl) {
 ?>
 <!DOCTYPE HTML>
 <html lang="fr">
@@ -31,47 +31,71 @@ function content_5889c316853448_89768585 (Smarty_Internal_Template $_smarty_tpl)
 	</head>	
 	<body>	
         <div id="page">
-            <h1>Silicon home</h1>
+            <header>
+                <div class="menuBox">
 
-
-
-    <?php
+                    <div class="left">
+                        <a href="#">
+                            <h1>SiliconApp</h1>
+                            <div>a product by Software Company</div>
+                        </a>
+                    </div>
+                    <div class="right">
+                        <ul>
+                            <?php
 $__section_sec1_0_saved = isset($_smarty_tpl->tpl_vars['__smarty_section_sec1']) ? $_smarty_tpl->tpl_vars['__smarty_section_sec1'] : false;
 $__section_sec1_0_loop = (is_array(@$_loop=$_smarty_tpl->tpl_vars['menu']->value) ? count($_loop) : max(0, (int) $_loop));
 $__section_sec1_0_total = $__section_sec1_0_loop;
 $_smarty_tpl->tpl_vars['__smarty_section_sec1'] = new Smarty_Variable(array());
 if ($__section_sec1_0_total != 0) {
 for ($__section_sec1_0_iteration = 1, $_smarty_tpl->tpl_vars['__smarty_section_sec1']->value['index'] = 0; $__section_sec1_0_iteration <= $__section_sec1_0_total; $__section_sec1_0_iteration++, $_smarty_tpl->tpl_vars['__smarty_section_sec1']->value['index']++){
-?>            
-        <div>
-            <form method="post" action="./admin.php?page=menu">
-                <input type="hidden" name="id" value="<?php echo $_smarty_tpl->tpl_vars['menu']->value[(isset($_smarty_tpl->tpl_vars['__smarty_section_sec1']->value['index']) ? $_smarty_tpl->tpl_vars['__smarty_section_sec1']->value['index'] : null)]['id'];?>
-" />
-                <input type="hidden" name="type" value="editer" />
-                <input type="number" name="position" min="1" max="20" value="<?php echo $_smarty_tpl->tpl_vars['menu']->value[(isset($_smarty_tpl->tpl_vars['__smarty_section_sec1']->value['index']) ? $_smarty_tpl->tpl_vars['__smarty_section_sec1']->value['index'] : null)]['position'];?>
-" />
-                <input type="text" name="name" value="<?php echo $_smarty_tpl->tpl_vars['menu']->value[(isset($_smarty_tpl->tpl_vars['__smarty_section_sec1']->value['index']) ? $_smarty_tpl->tpl_vars['__smarty_section_sec1']->value['index'] : null)]['name'];?>
-" />
-                <input type="image" src="../../img/edit.png">
-            </form>
-            <form method="post" action="./admin.php?page=menu">
-                <input type="hidden" name="id" value="<?php echo $_smarty_tpl->tpl_vars['menu']->value[(isset($_smarty_tpl->tpl_vars['__smarty_section_sec1']->value['index']) ? $_smarty_tpl->tpl_vars['__smarty_section_sec1']->value['index'] : null)]['id'];?>
-" />
-                <input type="hidden" name="type" value="supprimer" />
-                <input type="image" src="../../img/delete.png">
-            </form>
-        </div>
-    <?php
+?>
+                                <a href="#"><li><?php echo $_smarty_tpl->tpl_vars['menu']->value[(isset($_smarty_tpl->tpl_vars['__smarty_section_sec1']->value['index']) ? $_smarty_tpl->tpl_vars['__smarty_section_sec1']->value['index'] : null)]['name'];?>
+</li></a>
+                            <?php
 }
 }
 if ($__section_sec1_0_saved) {
 $_smarty_tpl->tpl_vars['__smarty_section_sec1'] = $__section_sec1_0_saved;
 }
 ?>
-
-
-
-
+                        </ul>
+                    </div>
+                    <div class="clear"></div>
+                </div>
+            </header>
+            <div class="imageBox">
+                imageBox
+            </div>
+            <div class="downloadBox">downloadBox</div>
+            <div class="descriptionBox">descriptionBox</div>
+            <footer>
+                <div class="left">
+                    <ul>
+                        <?php
+$__section_sec1_1_saved = isset($_smarty_tpl->tpl_vars['__smarty_section_sec1']) ? $_smarty_tpl->tpl_vars['__smarty_section_sec1'] : false;
+$__section_sec1_1_loop = (is_array(@$_loop=$_smarty_tpl->tpl_vars['menu']->value) ? count($_loop) : max(0, (int) $_loop));
+$__section_sec1_1_total = $__section_sec1_1_loop;
+$_smarty_tpl->tpl_vars['__smarty_section_sec1'] = new Smarty_Variable(array());
+if ($__section_sec1_1_total != 0) {
+for ($__section_sec1_1_iteration = 1, $_smarty_tpl->tpl_vars['__smarty_section_sec1']->value['index'] = 0; $__section_sec1_1_iteration <= $__section_sec1_1_total; $__section_sec1_1_iteration++, $_smarty_tpl->tpl_vars['__smarty_section_sec1']->value['index']++){
+?>
+                            <a href="#"><li><?php echo $_smarty_tpl->tpl_vars['menu']->value[(isset($_smarty_tpl->tpl_vars['__smarty_section_sec1']->value['index']) ? $_smarty_tpl->tpl_vars['__smarty_section_sec1']->value['index'] : null)]['name'];?>
+</li></a>
+                        <?php
+}
+}
+if ($__section_sec1_1_saved) {
+$_smarty_tpl->tpl_vars['__smarty_section_sec1'] = $__section_sec1_1_saved;
+}
+?>
+                    </ul>
+                </div>
+                <div class="right">
+                    2009 Software Company. All rights reserved
+                </div>
+                <div class="clear"></div>
+            </footer>
         </div>
 	</body>
 </html><?php }
--- 24_Silicon/model/site/home.php
+++ 24_Silicon/model/site/home.php
@@ -1,18 +1,4 @@
 <?php
-    // // Ajout d'un nouveau menu
-    // if(isset($_POST['type']) && $_POST['type'] == 'ajouter'){
-    //     echo 'INSERT INTO `menu` SET `name`="ajout"';
-    //     mysqli_query($link, 'INSERT INTO `menu` SET `name`="ajout"');
-    // }
-    // // supression
-    // if (isset($_POST['type']) && $_POST['type'] == 'supprimer'){
-    //     mysqli_query($link, 'DELETE FROM `menu` WHERE `id`='.$_POST['id']);
-    // }
-    // // edition
-    // if (isset($_POST['type']) && $_POST['type'] == 'editer'){ 
-    //     mysqli_query($link, 'UPDATE `menu` SET `position`="'.$_POST['position'].'", `name`="'.$_POST['name'].'" WHERE `id`='.$_POST['id']);
-    // }
-    // affichage liste
     $resultat = mysqli_query($link, 'SELECT * FROM `menu` ORDER BY `position` DESC, `name`');
     $assignTable = [];
     while($data = mysqli_fetch_assoc($resultat)){ // tant que tu reçois des données tu les affiches
